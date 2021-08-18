e55a00c3eb5627d834a7a3eb7b0a016d5d18ec11
augustransnas <august.ransnas@tele2.com>
2021-08-18 15:50:56 +0200

    just playing around

--- App.js
+++ App.js
@@ -1,21 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import React, {useState} from 'react';
+import {Modal, Pressable, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [numberOfPlayersModalVisible, setNumberOfPlayersModalVisible] = useState(false);
+    const [numberOfPlayers, setNumberOfPlayers] = useState(2);
+
+    return (
+        <View style={styles.container}>
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={numberOfPlayersModalVisible}
+                onRequestClose={() => {
+                    setNumberOfPlayersModalVisible(!numberOfPlayersModalVisible);
+                }}
+                supportedOrientations={['portrait', 'landscape']}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>Select number players.</Text>
+                        {[1, 2, 3, 4].map((selectableNumber) => (
+                            <Pressable
+                                key={selectableNumber}
+                                style={[styles.button, styles.selectableNumberOfPlayers]}
+                                onPress={() => {
+                                    setNumberOfPlayersModalVisible(!numberOfPlayersModalVisible);
+                                    setNumberOfPlayers(selectableNumber);
+                                }}
+                            >
+                                <Text style={styles.textStyle}>{selectableNumber}</Text>
+                            </Pressable>
+                        ))}
+                    </View>
+                </View>
+            </Modal>
+            <Text>mtg-madness</Text>
+            <Text>{numberOfPlayers}</Text>
+            <Pressable
+                style={[styles.button, styles.buttonOpen]}
+                onPress={() => setNumberOfPlayersModalVisible(true)}
+            >
+                <Text style={styles.textStyle}>Game menu</Text>
+            </Pressable>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2
+    },
+    buttonOpen: {
+        backgroundColor: "#00b8ae",
+    },
+    selectableNumberOfPlayers: {
+        backgroundColor: "#00b8ae",
+    },
+    textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: "center"
+    }
 });
